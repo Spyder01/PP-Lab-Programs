c00ca43a9faa527abb6c0a27f3ee051ebf31f00c
Spyder01 <suhan01.bangera@gmail.com>
2023-01-16 12:09:19 +0530

    Added openMP program 4

---
+++ open_mp/omp_4.c
@@ -0,0 +1,33 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int fib(int n) {
+  int a = 0, b = 1, t;
+#pragma omp parallel for schedule(static, 2)
+  for (int i = 0; i < n; i++) {
+#pragma omp critical
+    {
+      t = a + b;
+      a = b;
+      b = t;
+    }
+  }
+  return a;
+}
+
+
+int main(int argc, char *argv[]) {
+  int n = 20;
+  double start = omp_get_wtime();
+#pragma omp parallel for
+  for (int i = 0; i < n; i++) {
+    int t = omp_get_thread_num();
+    printf("thread: %d fib(%d) = %d\n", t, i, fib(i));
+  }
+  double end = omp_get_wtime();
+  printf("using schedule time is : %f\n", end - start);
+
+  return 0;
+}
