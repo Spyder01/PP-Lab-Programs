79378dde1a9ee1630bf27a51f8f81ac3406b972b
Spyder01 <suhan01.bangera@gmail.com>
2023-01-16 19:24:54 +0530

    Added openMP program 4

--- MPI/mpi_5.c
+++ MPI/mpi_5.c
@@ -1,51 +1,48 @@
-#include<stdio.h>
-#include<mpi.h>
+#include <mpi.h>
+#include <stdio.h>
 
-int isPrime (int x) {
+int isPrime(int x) {
 
-	for (int i=2; i*i<x; i++) {
-		if (x%i == 0) {
-			return 0;
-		}
-	}
+  for (int i = 2; i * i <= x; i++) {
+    if (x % i == 0) {
+      return 0;
+    }
+  }
 
-	return 1;
+  return 1;
 }
 
-int main (int argn, char** argc) {
-	int comm_sz, my_rank;
+int main(int argn, char **argc) {
+  int comm_sz, my_rank;
 
-	int n, root=0;
+  int n, root = 0;
 
-	MPI_Init (&argn, &argc);
-	MPI_Comm_size (MPI_COMM_WORLD, &comm_sz);
-	MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+  MPI_Init(&argn, &argc);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	if (my_rank == root) {
-		printf ("Enter the value of n \n");
-		scanf("%d", &n);
-	}
+  if (my_rank == root) {
+    printf("Enter the value of n \n");
+    scanf("%d", &n);
+  }
 
-	MPI_Bcast (&n, 1, MPI_INT, root, MPI_COMM_WORLD); 
-	
-	int interval = n/comm_sz;
+  MPI_Bcast(&n, 1, MPI_INT, root, MPI_COMM_WORLD);
 
-	int start = my_rank*interval+1;
-	int end = (my_rank+1)*interval;
+  int interval = n / comm_sz;
 
-	if (my_rank == root) {
-		printf("Printing Prime numbers below %d \n", n);
-	}
+  int start = my_rank * interval + 1;
+  int end = (my_rank + 1) * interval;
 
-	for (int i=start; i<end; i++) {
-		if (i>1 && isPrime(i)) {
-			printf ("%d ", i);
-		}
-	}
+  if (my_rank == root) {
+    printf("Printing Prime numbers below %d \n", n);
+  }
 
-	
+  for (int i = start; i <= end; i++) {
+    if (i > 1 && isPrime(i)) {
+      printf("%d ", i);
+    }
+  }
 
-	MPI_Finalize ();
-	return 0;
+  MPI_Finalize();
+  return 0;
 }
-
